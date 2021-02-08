50bfa32a73ce040822eff0c565d16e1d6fdbfa6d
Jan Knedlik <j.knedlik@gsi.de>
2021-02-08 20:19:45 +0100

    mv Algorithms into new class with config options

--- examples/geneva/16_GGenericStarter/GGenericStarter.cpp
+++ examples/geneva/16_GGenericStarter/GGenericStarter.cpp
@@ -1,52 +1,10 @@
-/**
- * @file GFunctionMinimizer.cpp
- */
-
-/********************************************************************************
- *
- * This file is part of the Geneva library collection. The following license
- * applies to this file:
- *
- * ------------------------------------------------------------------------------
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ------------------------------------------------------------------------------
- *
- * Note that other files in the Geneva library collection may use a different
- * license. Please see the licensing information in each file.
- *
- ********************************************************************************
- *
- * Geneva was started by Dr. Rüdiger Berlich and was later maintained together
- * with Dr. Ariel Garcia under the auspices of Gemfony scientific. For further
- * information on Gemfony scientific, see http://www.gemfomy.eu .
- *
- * The majority of files in Geneva was released under the Apache license v2.0
- * in February 2020.
- *
- * See the NOTICE file in the top-level directory of the Geneva library
- * collection for a list of contributors and copyright information.
- *
- ********************************************************************************/
-
 // Standard header files go here
 #include <iostream>
 
-// Geneva header files go here
-#include <geneva/GPluggableOptimizationMonitors.hpp>
-
-// The individual that should be optimized
+// The new geneva optimizer interface #3
 #include <geneva-interface/Go3.hpp>
 
+// The lambda to be used
 auto lambda = [](std::vector<double>& a) {
   return std::accumulate(a.begin(), a.end(), 0.0);
 };
@@ -54,28 +12,24 @@ auto lambda = [](std::vector<double>& a) {
 int main(int argc, char** argv)
 {
   using namespace GO3;
-  /*Set start value, left &right bounds*/
+  /*Set start value, left & right bounds*/
   std::vector<double> start{50, 50, 50}, left{0, 0, 0}, right{100, 100, 100};
-  /*create optimizer and register Algorithms in order*/
-  // Id be happy using nice designated initializers of c++20
-  GenevaOptimizer3 go3{argc, argv, .algos = {Algorithm::EA, Algorithm::GD}};
-  /*        v---------|tweak that algorithms config easily from here*/
-  // go3.algorithm(0).config[""] = 2;
-  auto pop = Population(
-      start, left, right,
-      std::forward<decltype(lambda)>(lambda));	// Now we only have to remove
-						// this forward<>()...
-  /* change population config,mutationprob,etc. */
+  /*create optimizer,give CLI options and register algorithms in order*/
 
+  Algorithm_EA ea{.Iterations = 30};
+  ea[cfg::Iterations] = 40;
+  GenevaOptimizer3 go3{argc, argv};
+  /*        v---------|tweak that algorithms config easily from here*/
+  // auto ea = algorithm::EvolutionaryAlgorithm();
+  // ea[cfg::Iterations] = 100;  // only accepts cfg's for EA
+  auto pop = Population{start, left, right, .func = lambda,
+			.Iterations = {10}};  // Now we only have to remove
+  /* change population config,mutationprob,etc. */
+  // pop[cfg::size] = 1000;
+  pop[cfg::Iterations] = 10;
+  // ea[cfg::Iterations] = 100;  // only accepts cfg's for EA
   /* optimize */
-  std::cout << "pop" << pop[cfg::Iterations] << std::endl;
-  pop[cfg::Iterations] = 5;
-  std::cout << "pop" << pop[cfg::Iterations] << std::endl;
-  auto best_ptr = go3.optimize(pop);
-  /* alternatively like this*/
+  auto best_ptr = go3.optimize(pop, {ea, Algorithm_GD{.Iterations = 100}});
   // auto best_ptr = go->optimize(start, left, right, lambda);  // take Pop&
   std::cout << best_ptr << std::endl;
-  std::cout << "Minsigma is" << pop.factory_ptr->getMinSigma() << std::endl;
-  std::cout << "Minsigma is" << pop.factory_ptr->getMinSigma() << std::endl;
-  // std::cout << "MINSIGMA is" << std::pop["minSigma"] << std::endl;
 }
--- include/geneva-interface/Go3.hpp
+++ include/geneva-interface/Go3.hpp
@@ -4,7 +4,7 @@
 #include "geneva-interface/GenericOptimizer.hpp"
 namespace GO3 {
 
-enum Algorithm { EA, GD };
+enum Algorithms { EA, GD };
 namespace cfg {
 template <size_t I>
 using ic = std::integral_constant<std::size_t, I>;
@@ -12,60 +12,78 @@ static inline constexpr ic<0> Iterations{};
 static inline constexpr ic<1> test{};
 static inline constexpr ic<2> x{};
 }  // namespace cfg
-template <typename Function>
-class Population {
-  std::tuple<int, std::string, double> cfg{42, "test", 0.1};
-
+template <typename... Ts>
+class integralconfig {
+  protected:
   public:
-  std::shared_ptr<GenericIndividualFactory<Function>> factory_ptr;
-
-  using Individual_t = GenericIndividual<Function>;
-  Population(std::vector<double> start, std::vector<double> left,
-	     std::vector<double> right, Function&& func)
-      : factory_ptr(new GenericIndividualFactory<Function>(
-	    "config/GenericIndividual.json", start, left, right,
-	    std::forward<decltype(func)>(func)))
-  {
-    GenericIndividual<Function>::setFunc(std::forward<Function>(func));
-  }
-
+  std::tuple<Ts...> cfg;
   template <typename Integral>
   constexpr auto& operator[](Integral)
   {
     return std::get<Integral::value>(cfg);
   }
+  integralconfig(std::tuple<Ts...> ts) : cfg(ts){};
 };
+using popconf = integralconfig<int, std::string, double>;
+template <typename Function>
+class Population : public popconf {
+  public:
+  std::shared_ptr<GenericIndividualFactory<Function>> factory_ptr;
+  using Individual_t = GenericIndividual<Function>;
+  Population(std::vector<double> start, std::vector<double> left,
+	     std::vector<double> right, Function& func, int Iterations = 42)
+      : Population(start, left, right, std::forward<Function>(func), Iterations)
+  {
+  }
+  Population(std::vector<double> start, std::vector<double> left,
+	     std::vector<double> right, Function&& func, int Iterations = 42)
+      : factory_ptr(new GenericIndividualFactory<Function>(
+	    "config/GenericIndividual.json", start, left, right,
+	    std::forward<decltype(func)>(func))),
+	popconf({Iterations, "testfile", 0.0})
+  {
+    GenericIndividual<Function>::setFunc(std::forward<Function>(func));
+  }
+};
+using algoconf = integralconfig<int, int>;
+
+template <typename FactoryT, typename AlgoT>
+class Algorithm : public algoconf {
+  public:
+  Algorithm(int Iterations = 5) : algoconf({Iterations, 2}) {}
+  std::shared_ptr<AlgoT> get()
+  {
+    std::shared_ptr<AlgoT> algo = FactoryT().template get<AlgoT>();
+    algo->setMaxIteration((*this)[cfg::Iterations]);
+    return algo;
+  }
+  void addToOptimization(Gem::Geneva::Go2& go)
+  {
+    std::shared_ptr<AlgoT> algo = FactoryT().template get<AlgoT>();
+    algo->setMaxIteration((*this)[cfg::Iterations]);
+    go& algo;
+  };
+};
+using Algorithm_EA = Algorithm<Gem::Geneva::GEvolutionaryAlgorithmFactory,
+			       Gem::Geneva::GEvolutionaryAlgorithm>;
+using Algorithm_GD = Algorithm<Gem::Geneva::GGradientDescentFactory,
+			       Gem::Geneva::GGradientDescent>;
+
 class GenevaOptimizer3 {
-  std::vector<Algorithm> algos;
+  using algorithmsT = std::vector<std::variant<Algorithm_EA, Algorithm_GD>>;
 
   public:
-  int argc;
-  char** argv;
   Gem::Geneva::Go2 go;
-  GenevaOptimizer3(int argc, char** argv,
-		   std::vector<Algorithm> algos = {Algorithm::EA})
-      : algos(algos),
-	argc(argc),
-	argv(argv),
-	go(argc, argv, "config/Go2.json"){};
+  GenevaOptimizer3(int argc, char** argv) : go(argc, argv, "config/Go2.json"){};
 
   template <typename Population>
-  auto optimize(Population& pop)
+  auto optimize(Population& pop, algorithmsT algos = {Algorithm_EA()})
   {
-    if (go.clientMode()) {
-      go.clientRun();
-    }
-    for (auto algo : algos) {
-      switch (algo) {
-	case Algorithm::EA:
-	  go& Gem::Geneva::GEvolutionaryAlgorithmFactory()
-	      .get<GEvolutionaryAlgorithm>();
-	  break;
-	case Algorithm::GD:
-	  go& Gem::Geneva::GGradientDescentFactory().get<GGradientDescent>();
-	  break;
-      }
-    }
+    if (go.clientMode()) go.clientRun();
+
+    for (auto algo : algos) std::visit([&](auto& al) { go& al.get(); }, algo);
+    // al.addToOptimization(go); }, algo);
+
     go.registerContentCreator(pop.factory_ptr);
     return go.optimize()
 	->getBestGlobalIndividual<typename Population::Individual_t>();
